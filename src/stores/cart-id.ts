2de98a4240d24d60769e926baccdb695c674fecd
jycouet <jycouet@gmail.com>
2022-04-08 22:35:46 +0200

    :bug: FIX: localstorage is present only in browser

--- src/stores/cart-id.ts
+++ src/stores/cart-id.ts
@@ -1,8 +1,13 @@
+import { browser } from "$app/env";
 import { writable } from "svelte/store";
 import { v4 as uuidv4 } from "uuid";
 
-const existingCartId = localStorage.getItem("kitql-cart-id");
+const existingCartId = browser ? localStorage.getItem("kitql-cart-id") : null;
 
 export const cartId = writable(existingCartId || uuidv4());
 
-cartId.subscribe((id) => localStorage.setItem("kitql-cart-id", id));
+cartId.subscribe((id) => {
+  if (browser) {
+    localStorage.setItem("kitql-cart-id", id);
+  }
+});
