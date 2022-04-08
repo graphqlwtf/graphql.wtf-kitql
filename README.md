70bdfec52fca91a77c8a48494e493337e0b43237
jycouet <jycouet@gmail.com>
2022-04-08 22:34:38 +0200

    :pencil2: IMPROVE: onboarding

---
+++ README.md
@@ -0,0 +1,9 @@
+# graphql.wtf-kitql
+
+1/ clone the repo
+
+2/ create `.env` from `.env.example` ad fill values
+
+3/ install `npm i`
+
+4/ run `npm run dev`
--- src/lib/graphql/kitQLClient.ts
+++ src/lib/graphql/kitQLClient.ts
@@ -1,8 +1,9 @@
 import { KitQLClient } from "@kitql/client";
-const GRAPHQL_ENDPOINT = import.meta.env.VITE_GRAPHQL_ENDPOINT;
+
+const url = import.meta.env.VITE_GRAPHQL_ENDPOINT;
 
 export const kitQLClient = new KitQLClient({
-  url: GRAPHQL_ENDPOINT as string,
+  url,
   credentials: "omit",
   headersContentType: "application/json",
   logType: ["client", "server", "operationAndvariables"],
